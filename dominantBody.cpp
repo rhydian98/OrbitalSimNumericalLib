1c7495568a8c947c8079a8a3861ace256b744b12
rhydian98 <rhydian98@gmail.com>
2026-09-15 06:42:54 +0100

    Added massive body struct and dominant body files

---
+++ dominantBody.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rhydian on 13/09/2026.
+//
+#include "dominantBody.h"
+
---
+++ massiveBody.h
@@ -0,0 +1,20 @@
+//
+// Created by rhydian on 13/09/2026.
+//
+
+#ifndef PURNELLNUMERICAL_MASSIVEBODY_H
+#define PURNELLNUMERICAL_MASSIVEBODY_H
+#include <string>
+#include "library.h"
+struct MassiveBody {
+
+    std::string name;
+    double u;
+    Vector3 position;
+
+
+};
+
+
+
+#endif //PURNELLNUMERICAL_MASSIVEBODY_H
